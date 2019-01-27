9dad1f0210df773a3831f0ecb6dcca3e4b203aff
miniweb1 <miniweb@abv.bg>
2019-01-27 11:15:20 +0200

    Sync

--- includes/functions.php
+++ includes/functions.php
@@ -1,21 +1,22 @@
 <?php
 
 function stringChainReplacements($stringArray) {
+    // Check if the string array have only 1 element
     if(count($stringArray) <= 1) {
         return 0;
     }
     
+    // Init counter
     $counter = 0;
-    $c = 0;
+
     for($i = 0; $i < count($stringArray) - 1; $i += 1) {
+        // substr($stringArray[$i], 0, 1)
         $first = $stringArray[$i + 1][0];
+        // substr($stringArray[$i], -1)
         $last = $stringArray[$i][-1];
         if($last !== $first) {
             $counter += 1;
-        }  
+        }
+    }
     return $counter;
 }
-
-echo stringChainReplacements(["abc", 
-"def", 
-"ghi"]);
--- index.php
+++ index.php
@@ -1,33 +1,33 @@
-<?php include './functions.php'; ?>
+<?php include './includes/functions.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/vendor/normalize.css">
+    <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
-    <?php include './includes/header.php'; ?>
-    <main>
-        <form action="index.php" method="GET">
-            <label>Масив от стрингове:</label>
-            <input type="text" name="string_array">
-            <button type="submit">Изчисли</button>
-        </form>
-        <p>Въведете отделните елементи от масива със запетайка и интервал след това. Не слагайте кавички на отделните стрингове!</p>
-        <?php
-            if(!empty($_GET)) {
-                $stringArray = $_GET['string_array'];
-                $stringArray = explode(', ', $stringArray);
-                if($stringArray === '') {
-                    echo 'err';
-                } else {
-                    echo stringChainReplacements($stringArray);
+    <div id="container">
+        <?php include './includes/header.php'; ?>
+        <main>
+            <form action="index.php" method="GET">
+                <label>Масив от стрингове:</label>
+                <input type="text" pattern="(([a-z]{1,}), ){1,}[a-z]{1,}" name="string_array">
+                <button type="submit">Изчисли</button>
+            </form>
+            <p class="italic">Въведете отделните елементи от масива със запетайка и интервал след това. Не слагайте кавички на отделните стрингове!</p>
+            <?php
+                if(!empty($_GET)) {
+                    $stringArray = $_GET['string_array'];
+                    $stringArray = explode(', ', $stringArray);
+                    echo 'Резултат: ' . stringChainReplacements($stringArray);
                 }
-            }
-        ?>
-    </main>
-    <?php include './includes/footer.php'; ?>
+                ?>
+        </main>
+        <?php include './includes/footer.php'; ?>
+    </div>
 </body>
 </html>
