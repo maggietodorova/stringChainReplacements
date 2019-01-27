b0911a51387350823fcc5cfba5723d0f5fba4333
miniweb1 <miniweb@abv.bg>
2019-01-27 12:07:12 +0200

    Minor changes

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// https://app.codesignal.com/challenge/ZFpmAs8w74eHvoHbA
+
 function stringChainReplacements($stringArray) {
     // Check if the string array have only 1 element
     if(count($stringArray) <= 1) {
@@ -10,13 +12,19 @@ function stringChainReplacements($stringArray) {
     $counter = 0;
 
     for($i = 0; $i < count($stringArray) - 1; $i += 1) {
-        // substr($stringArray[$i], 0, 1)
-        $first = $stringArray[$i + 1][0];
+        // Last letter of string
         // substr($stringArray[$i], -1)
         $last = $stringArray[$i][-1];
+        
+        // First letter of the next string
+        // substr($stringArray[$i], 0, 1)
+        $first = $stringArray[$i + 1][0];
         if($last !== $first) {
             $counter += 1;
         }
     }
     return $counter;
 }
+
+
+'fkkkf'[-1]
